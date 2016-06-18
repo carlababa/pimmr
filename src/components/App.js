af8ccaba45ab9c77b17b3d01801a627177a3b872
Carla Babadopulos <cacababa@gmail.com>
2016-06-19 00:12:19 +0200

    Create request and render a list with restaurants names

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import $ from 'jquery';
 import cssmodules from 'react-css-modules';
 import styles from './app.cssmodule.css';
 
 @cssmodules(styles)
 class AppComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      restaurants: []
+    };
+
+    this.getRestaurants(0);
+  }
+
+  getRestaurants(start = 0) {
+    const city = 'Amsterdam';
+    const url = 'https://api.pimmr.me';
+    const limit = 5;
+
+    $.ajax(url, {
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'restaurant.getHighestRated',
+        params: [city, start, limit],
+        id: 0,
+      })
+    })
+    .then(response => {
+      this.setState({ restaurants: response.result });
+    });
+  }
 
   render() {
     return (
-
+      <div>
+        <h2>Restaurants</h2>
+        <ul>
+          {this.state.restaurants.map((restaurant) => (
+            <li>
+              <p>{restaurant.name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
